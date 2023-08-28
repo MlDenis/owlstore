197ed35cce8e0878e70582278575a9ba75f0a7aa
Maria <zaush@bk.ru>
2023-08-28 14:14:20 +0300

    added tests for LoadOrderNumber()

--- go.mod
+++ go.mod
@@ -12,19 +12,27 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/mock v1.6.0
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/go-chi/chi v1.5.4 // indirect
-	github.com/go-chi/httprate v0.7.4 // indirect
-	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/go-chi/httprate v0.7.4
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lib/pq v1.10.2 // indirect
+	github.com/stretchr/testify v1.8.4
 	go.uber.org/atomic v1.7.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
---
+++ internal/gofermart/handlers/orders_test.go
@@ -0,0 +1,202 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/MlDenis/internal/gofermart/auth"
+	"github.com/MlDenis/internal/gofermart/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadOrderNumber_OrderAccepted(t *testing.T) {
+	data := struct {
+		name       string
+		request    string
+		method     string
+		body       string
+		user       models.UserData
+		statusCode int
+	}{
+		name:    "new order has been accepted",
+		request: "/api/user/orders",
+		method:  http.MethodPost,
+		body:    "9278923470",
+		user: models.UserData{
+			Login:        "test",
+			Password:     "test",
+			PasswordHash: auth.HashPassword("test"),
+			Token:        "",
+		},
+		statusCode: http.StatusAccepted,
+	}
+
+	r, ctrl, handler, storeInterface := runTestServer(t)
+	defer ctrl.Finish()
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	body := strings.NewReader(string(data.body))
+
+	err := authUser(&data.user, handler)
+	require.NoError(t, err)
+
+	order, err := makeOrder(data.user.Login, data.body)
+	require.NoError(t, err)
+
+	ctx := context.TODO()
+
+	storeInterface.EXPECT().LoadOrderInDB(ctx, order)
+
+	resp := testRequest(t, ts, data.method, data.request, body, data.user.Token)
+	defer resp.Body.Close()
+
+	assert.Equal(t, data.statusCode, resp.StatusCode)
+
+}
+
+func makeOrder(login string, order string) (*models.Orders, error) {
+	orderID, err := strconv.ParseInt(order, 10, 64)
+	if err != nil {
+		return &models.Orders{}, err
+	}
+
+	orderModel := &models.Orders{
+		UserLogin:   login,
+		OrderNumber: orderID,
+	}
+
+	return orderModel, nil
+}
+
+func authUser(user *models.UserData, handler *HandlerDB) error {
+	token, err := auth.CreateJwtToken(user.Login)
+	if err != nil {
+		return err
+	}
+
+	user.Token = token
+
+	handler.DataJWT.AddToken(user)
+
+	return nil
+}
+
+func TestLoadOrderNumber_UnauthorizedUser(t *testing.T) {
+	data := struct {
+		name       string
+		request    string
+		method     string
+		body       string
+		user       models.UserData
+		statusCode int
+	}{
+		name:       "unauthorized user",
+		request:    "/api/user/orders",
+		method:     http.MethodPost,
+		body:       "9278923470",
+		user:       models.UserData{},
+		statusCode: http.StatusUnauthorized,
+	}
+
+	r, ctrl, _, _ := runTestServer(t)
+
+	defer ctrl.Finish()
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	var token string
+	body := strings.NewReader(string(data.body))
+
+	resp := testRequest(t, ts, data.method, data.request, body, token)
+	defer resp.Body.Close()
+
+	assert.Equal(t, data.statusCode, resp.StatusCode)
+}
+
+func TestLoadOrderNumber_WrongOrderNumber(t *testing.T) {
+	data := struct {
+		name       string
+		request    string
+		method     string
+		body       string
+		user       models.UserData
+		statusCode int
+	}{
+		name:    "wrong order number",
+		request: "/api/user/orders",
+		method:  http.MethodPost,
+		body:    "test",
+		user: models.UserData{
+			Login:        "test",
+			Password:     "test",
+			PasswordHash: auth.HashPassword("test"),
+			Token:        "",
+		},
+		statusCode: http.StatusBadRequest,
+	}
+
+	r, ctrl, handler, _ := runTestServer(t)
+
+	defer ctrl.Finish()
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	body := strings.NewReader(string(data.body))
+
+	err := authUser(&data.user, handler)
+	require.NoError(t, err)
+
+	resp := testRequest(t, ts, data.method, data.request, body, data.user.Token)
+	defer resp.Body.Close()
+
+	assert.Equal(t, data.statusCode, resp.StatusCode)
+}
+
+func TestLoadOrderNumber_InvalidOrderNumber(t *testing.T) {
+	data := struct {
+		name       string
+		request    string
+		method     string
+		body       string
+		user       models.UserData
+		statusCode int
+	}{
+		name:    "invalid order number",
+		request: "/api/user/orders",
+		method:  http.MethodPost,
+		body:    "123",
+		user: models.UserData{
+			Login:        "test",
+			Password:     "test",
+			PasswordHash: auth.HashPassword("test"),
+			Token:        "",
+		},
+		statusCode: http.StatusUnprocessableEntity,
+	}
+
+	r, ctrl, handler, _ := runTestServer(t)
+
+	defer ctrl.Finish()
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	body := strings.NewReader(string(data.body))
+
+	err := authUser(&data.user, handler)
+	require.NoError(t, err)
+
+	resp := testRequest(t, ts, data.method, data.request, body, data.user.Token)
+	defer resp.Body.Close()
+
+	assert.Equal(t, data.statusCode, resp.StatusCode)
+}
